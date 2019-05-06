a3bf7d3fe0e483c225a23b2816d359ceb15a9a97
Zijad Aljic <zijad.aljic@jambit.com>
2019-05-06 16:47:30 +0200

    configured provisioning of nginx with ansible and packer

--- common/scripts/ubuntu/base.sh
+++ common/scripts/ubuntu/base.sh
@@ -8,7 +8,7 @@ export DEBIAN_FRONTEND=noninteractive
 apt-get -yq purge lxcfs unattended-upgrades
 apt-get update -y
 apt-get upgrade -y
-apt-get install -y awscli jq unzip nano curl bash-completion apt-transport-https
+apt-get install -y awscli jq unzip nano curl bash-completion apt-transport-https python
 apt-get -yq autoremove snapd
 
 
