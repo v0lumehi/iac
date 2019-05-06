3c40fdfbdb806c2850ea32703b24b1af357d8a71
v0lumehi <aljic.zijad@gmail.com>
2019-05-06 12:15:43 +0000

    changed build.json and modified install_nginx.sh to create nginx ami with packer

--- static_webserver/scripts/install_nginx.sh
+++ static_webserver/scripts/install_nginx.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/env bash
 
 # Do installation of nginx here
+sudo bash -e <<SCRIPT
+DEBIAN_FRONTEND=noninteractive apt-get install -y nginx
+
+mv /tmp/index.html /var/www/html/
+
+chmod 655 /var/www/html/index.html
+
+systemctl restart nginx
+SCRIPT
